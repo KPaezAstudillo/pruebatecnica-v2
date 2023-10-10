b5affc394470c16332c564b1add78439e3824049
Karina Paez <karina@difusa.cl>
2023-10-10 16:02:20 -0300

    arreglo paquetes archivo package.json

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "dropify/dist/css/dropify.min.css";
-import "dropify/dist/js/dropify.min.js";
+
 
 import "./index.css";
 import App from "./App";
